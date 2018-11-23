c01346be32e6d5ea77016af838cf27107d82e441
Jordan Woodman <jordan.woodman@bluesombrero.com>
2018-11-23 10:03:19 -0500

    Add stats health check

--- overwatch-api/Controllers/HealthController.cs
+++ overwatch-api/Controllers/HealthController.cs
@@ -1,17 +1,73 @@
-﻿using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using overwatch_api.Enums;
+using overwatch_api.Models;
+using overwatch_api.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace overwatch_api.Controllers
 {
-    [DisableCors]
     [ApiController]
     [Route("api/[controller]")]
     public class HealthController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
+        private readonly ILogger<HealthController> _logger;
+        private readonly IEnumerable<IStatsService> _statsServices;
+
+        public HealthController(ILogger<HealthController> logger, IEnumerable<IStatsService> statsServices)
         {
-            return new OkObjectResult(new { status = "wearegoodtogo" });
+            _logger = logger;
+            _statsServices = statsServices;
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(ServiceStatus), 200)]
+        public OkObjectResult GetHealth()
+        {
+            return Ok(new ServiceStatus(Health.Healthy));
+        }
+
+        [HttpGet("Stats")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(IEnumerable<ServiceStatus>), 200)]
+        public async Task<OkObjectResult> GetServicesHealth([FromQuery] bool deep = false)
+        {
+            var statuses = new List<ServiceStatus>();
+
+            foreach(var service in _statsServices)
+            {
+                statuses.Add(await GetStatsServiceStatus(service, deep));
+            }
+
+            return Ok(statuses);
+        }
+
+        private async Task<ServiceStatus> GetStatsServiceStatus(IStatsService service, bool deep)
+        {
+            var status = new ServiceStatus(Health.Healthy, service.Name);
+
+            try
+            {
+                if(deep)
+                {
+                    await service.GetAsync(Platform.Pc, Region.Na, "woodman-11497");
+                }
+                else if (service.Disabled)
+                {
+                    status.Health = Health.Unavailable;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, $"{service.Name} is unavailable.");
+
+                status.Health = Health.Unavailable;
+            }
+
+            return status;
         }
     }
 }
---
+++ overwatch-api/Models/ServiceStatus.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace overwatch_api.Models
+{
+    public enum Health
+    {
+        Healthy,
+        Unavailable
+    }
+
+    public class ServiceStatus
+    {
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        public Health Health { get; set; }
+
+        public string Status => Health == Health.Healthy ? "wearegoodtogo" : "unavailable";
+
+        public ServiceStatus(Health health, string name = null)
+        {
+            Health = health;
+            Name = name;
+        }
+
+        public bool ShouldSerializeName() => !string.IsNullOrEmpty(Name);
+    }
+}
